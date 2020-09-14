141a90707641f5ea19a9c266df9a05e0b6e61296
edgar alfaro <alfa-_01@outlook.es>
2020-09-13 21:07:47 -0500

    agrego commit inicial

---
+++ server/index.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config();
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6zk99.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
